3d131c42844a6f68b858fa0e9d339766e9f9c89c
Tri Denda <tridenda.nk@gmail.com>
2022-09-06 13:56:14 +0700

    added card component

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 
 import Banner from "../components/banner";
+import Card from "../components/card";
 
 import styles from "../styles/Home.module.css";
 
@@ -23,7 +24,33 @@ export default function Home() {
           handleOnClick={handleOnBannerBtnClick}
         />
         <div className={styles.heroImage}>
-          <Image src="/static/hero-image.png" width={700} height={400} />
+          <Image
+            src="/static/hero-image.png"
+            width={700}
+            height={400}
+            alt="hero image"
+          />
+        </div>
+
+        <div className={styles.cardLayout}>
+          <Card
+            className={styles.card}
+            name="DarkHorse Coffee"
+            imgUrl="/static/hero-image.png"
+            href="/coffee-store/darkhorse-coffee"
+          />
+          <Card
+            className={styles.card}
+            name="DarkHorse Coffee"
+            imgUrl="/static/hero-image.png"
+            href="/coffee-store/darkhorse-coffee"
+          />
+          <Card
+            className={styles.card}
+            name="DarkHorse Coffee"
+            imgUrl="/static/hero-image.png"
+            href="/coffee-store/darkhorse-coffee"
+          />
         </div>
       </main>
     </div>
